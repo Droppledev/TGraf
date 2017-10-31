f23d02ccb31d8434e43c6205634d26287736d9b7
Unknown <akbar.exp57@gmail.com>
2017-10-31 23:44:50 +0700

    Something left to be false
    Donot copy :)

--- main.cpp
+++ main.cpp
@@ -2,17 +2,20 @@
 #include <string.h>
 #include <vector>
 #include <limits.h>
+#include <string.h>
+
 using namespace std;
 
 class Graph{
 private:
     int holder;
     int besar;
-    vector<int>arr[100]; //menyimpan input array sebagai representasi adjacency matrix
+    int src;
+    vector<int>arr[1000]; //menyimpan input array sebagai representasi adjacency matrix
     bool undirected;    //penyimpan nilai apakah dia sebuah undirected atau tidak
     vector<int>track[100]; //penyimpan nanti track menuju index track[index] apa saja , dimasukkanny pake push_back
-    int dist[100];      //distance dari src ke index
-    bool visited[100];  //apakah dia sudah fix / sudah di kunjungi?
+    int dist[1000];      //distance dari src ke index
+    bool visited[1000];  //apakah dia sudah fix / sudah di kunjungi?
 public:
     Graph(){undirected=true;}
     ~Graph();
@@ -20,9 +23,18 @@ public:
         this->besar=besar;
     }
     int getBesar(){return besar;}
+    void setSrc(int src){this->src=src;}
+    int getSrc(){return src;}
     void setInput();
     bool isValid();
     void djikstra();
+    int searchLowCost();
+    void print(){
+        cout<<"Vertex \t Distance from source\n";
+        for (int i=0 ; i < besar ; ++i){
+            cout<<"  "<<i<<"\t\t"<<dist[i]<<endl;
+        }
+    }
 };
 
 
@@ -49,13 +61,45 @@ bool Graph::isValid(){
 
 }
 
+int Graph::searchLowCost(){
+    int mi=INT_MAX,index;
+    for (int j=0 ; j < besar ; ++j){
+        if (mi>=dist[j]&& !visited[j]){
+            mi=dist[j];
+            index=j;
+        }
+    }
+    return index;
+}
+
 void Graph::djikstra(){
+for (int i=0 ; i < besar ; ++i) dist[i]=INT_MAX;
+memset(visited,false, sizeof(visited));
 
+dist[src]=0; //jarak dari mulai ke source pasti 0
 
+int current_shortest;
 
+    for (int i=0 ;i < besar ; ++i){ //eror heres
+    current_shortest=searchLowCost(); //disini pencarian terpendeknya
+    visited[current_shortest]=true;
 
+        for (int j=0 ; j < besar ; ++j){
+                if (!visited[j] && arr[current_shortest][j]
+                    &&dist[current_shortest]!=INT_MAX && !dist[current_shortest]+arr[current_shortest][j]<dist[j]){
+                        dist[j]=dist[current_shortest]+arr[current_shortest][j]; //untuk menuju vertex ke j dari vertex current_shortest , perlu cost
+                                                                            //sebanyank arr[current][j]
+                                                                            //akan diupdate trus
+                }
+        }
+    }
 }
 
 int main(){
-
+ Graph *mainframe = new Graph;
+ mainframe->setBesar(9);
+ mainframe->setSrc(0);
+ mainframe->setInput();
+ mainframe->djikstra();
+ mainframe->print();
 }
